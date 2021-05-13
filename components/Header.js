32e332b7e1b98cf1f6fbf508f75cbfaf3ba02464
Filipe <filipe.b.aleixo@gmail.com>
2021-05-13 23:34:48 +0100

    Added microphone icon to header

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,26 @@
 import PropTypes from 'prop-types';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faGem from '@fortawesome/fontawesome-free-regular/faGem'
+import faMicrophone from '@fortawesome/fontawesome-free-solid/faMicrophoneAlt'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            {/*<span className="icon fa-diamond"></span>*/}
-            <FontAwesomeIcon icon={faGem} transform="grow-18" />
+            {/*<span className="icon fa-microphone"></span>*/}
+            <FontAwesomeIcon icon={faMicrophone} transform="grow-22" />
         </div>
         <div className="content">
             <div className="inner">
                 <h1>Vorder</h1>
-                <h3>Trade crypto as fast as you speak</h3>
+                <h3>Trade crypto at the speed of your voice</h3>
                 <h3>And never miss a trade again.</h3>
                 <p> From traders to traders </p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Overview</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Try Vorder</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About Us</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
