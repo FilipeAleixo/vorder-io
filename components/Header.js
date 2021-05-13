fbc09f3c0078275ac9cf8b065cfd7599a37b194d
Filipe <filipe.b.aleixo@gmail.com>
2021-05-13 19:20:50 +0100

    fix

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const Header = (props) => (
         <div className="content">
             <div className="inner">
                 <h1>Vorder</h1>
-                <p>From dozens of clicks to one voice command. Trade crypto as fast as you speak and never miss a trading opportunity again.</a> license.</p>
+                <p>From dozens of clicks to one voice command. Trade crypto as fast as you speak and never miss a trading opportunity again.</p>
             </div>
         </div>
         <nav>
