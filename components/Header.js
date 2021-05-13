b19997b0d2716419380dee216f096e6e1965168f
Filipe <filipe.b.aleixo@gmail.com>
2021-05-13 19:18:15 +0100

    Edited header

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Vorder</h1>
+                <p>From dozens of clicks to one voice command. Trade crypto as fast as you speak and never miss a trading opportunity again.</a> license.</p>
             </div>
         </div>
         <nav>
