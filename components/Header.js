84553e0dff6260d79dc18ab08c9d735549afc06c
Filipe <filipe.b.aleixo@gmail.com>
2021-05-14 15:33:56 +0100

    updated minimist package

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,17 @@
 import PropTypes from 'prop-types';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faMicrophone from '@fortawesome/fontawesome-free-solid/faMicrophoneAlt'
+import faMicrophone from '@fortawesome/fontawesome-free-solid/faMicrophone'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
             {/*<span className="icon fa-microphone"></span>*/}
-            <FontAwesomeIcon icon={faMicrophone} transform="grow-22" />
+            <FontAwesomeIcon icon={faMicrophone} transform="grow-15" />
         </div>
         <div className="content">
             <div className="inner">
                 <h1>Vorder</h1>
-                <h3>Trade crypto at the speed of your voice</h3>
-                <h3>And never miss a trade again.</h3>
-                <p> From traders to traders </p>
+                <p>Trade crypto at the speed of your voice and never miss a trade again.</p>
             </div>
         </div>
         <nav>
