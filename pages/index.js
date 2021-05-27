09661e8e64084996baffd2bb80319b08bb5d028f
Filipe <filipe.b.aleixo@gmail.com>
2021-05-27 15:07:04 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
 
-import { API } from 'aws-amplify';
+import { Amplify, API, withSSRContext } from "aws-amplify";
+import awsExports from "../aws-exports";
+Amplify.configure({ ...awsExports, ssr: true });
 
 class IndexPage extends React.Component {
     constructor(props) {
